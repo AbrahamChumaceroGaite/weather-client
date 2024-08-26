897f206db726715285d0fcd29beaa71dd25c5ea3
AbrahamChumaceroGaite <91038671+AbrahamChumaceroGaite@users.noreply.github.com>
2024-08-26 12:07:31 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   mapboxAccessToken: 'pk.eyJ1IjoiZGFudGVhYnJhaGFtMiIsImEiOiJjbGl1Mmg0dnMxamQ5M2ZudzB3bm01NnQ5In0.ElL-GieNaB6KaaIlsfeNKg',
-  apiUrl: 'https://api.weather.dev.dtt.tja.ucb.edu.bo/weather/api',
-  apiSocket: 'https://api.weather.dev.dtt.tja.ucb.edu.bo',
+  apiUrl: 'https://watchcloud.piensadiferente.net/weather/api',
+  apiSocket: 'https://watchcloud.piensadiferente.net',
   VAPID_PUBLIC_KEY: 'BG-g3iRrKPX4xinFhFZgPbVzhbwactIpiM3PgwUTItu-ISNTk4wwfIl4_riWWPYtqzjmZXYCDGJPn2B6jInA0T0'
 
 };
